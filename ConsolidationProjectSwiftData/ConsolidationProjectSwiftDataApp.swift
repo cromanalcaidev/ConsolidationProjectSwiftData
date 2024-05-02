f099ebb5997d3944ff758d35a1c5e1dff9c3e634
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-05-02 17:32:21 +0200

    Initial Commit

---
+++ ConsolidationProjectSwiftData/ConsolidationProjectSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsolidationProjectSwiftDataApp.swift
+//  ConsolidationProjectSwiftData
+//
+//  Created by Carlos Román Alcaide on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ConsolidationProjectSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
